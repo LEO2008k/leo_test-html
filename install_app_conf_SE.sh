5e9735f5a4af35ee2bb4c31ff35f852e0446f0e8
Levko Kravchuk <levkokravchuk@Levkos-MacBook-Pro.local>
2020-05-04 17:53:37 +0300

    add  var hostname

--- install_app_conf_SE.sh
+++ install_app_conf_SE.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 yum -y update
 yum -y install mc vim net-tools wget git httpd 
+hh=`hostname`
+
 echo "---------start httpd and add html page -------------"
+
 privateip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<html><body bgcolor=black><center><h1><p><font color=red> Levko  web server with private ip $privateip</h1></center></body></html>" > /var/www/html/index.html
+echo "<html><body bgcolor=black><center><h1><p><font color=red> Levko  web server with private ip $privateip and $hh </h1></center></body></html>" > /var/www/html/index.html
 systemctl enable --now httpd
 echo "UserData executed on $(date)" >>/var/www/html/log.txt
 echo "_________finish_install_____________"
